eabecfa20793ade95afb6a3dfd2058c5bac0ce66
lovee <lovee@YUN>
2023-04-15 18:02:02 +0900

    master

---
+++ project/src/Interface_ex/in1.java
@@ -0,0 +1,73 @@
+package Interface_ex;
+
+import java.util.Scanner;
+
+public class in1 {
+
+	public static void main(String[] args) {
+		
+		udgame1 ud = new udgame1();
+		ud.user();
+		
+
+	}
+}
+class udgame1 implements udgame{
+	Scanner sc;
+	int count = 0;
+	@Override
+	public void user() {
+		sc = new Scanner(System.in);
+		while(true) {
+			System.out.println("숫자를 입력해주세요");
+			int user = sc.nextInt();
+			
+			if(user > 10) {
+				System.out.println("10이하의 숫자만 입력해주세요");
+			}
+			else {
+				count++;
+				if(count >= 4) {
+					System.out.printf("정답을 맞추지 못했습니다.\n정답은: %d",ranNumber);
+					break;
+				}
+				String z = result(user,ranNumber);
+				
+				if(z.equals("")) {
+					System.out.println("정답입니다");
+					break;
+				}
+				else {
+					System.out.println(z);
+					continue;
+				}
+			}
+		}
+	}
+	
+	@Override
+	public String result(int user, int ranNumber) {
+		String msg = "";
+		
+		if(user < ranNumber) {
+			msg = "up";
+		}
+		else if(user > ranNumber) {
+			msg = "down";
+		}
+		return msg;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
